fa826971d34fe5ef64772c018c9bff25fa337b10
miau <a59619@alunos.ipb.pt>
2025-03-08 15:19:55 +0000

    Health system

---
+++ Assets/UnityTechnologies/StarterAsset/Art/Ruby/scripts/DamagedZone.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DamagedZone : MonoBehaviour
+{
+    void OnTriggerStay2D(Collider2D other)
+    {
+        PlayerController controller = other.GetComponent<PlayerController>();
+
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(-1);
+        }
+    }
+}
--- Assets/UnityTechnologies/StarterAsset/Art/Ruby/scripts/PlayerController.cs
+++ Assets/UnityTechnologies/StarterAsset/Art/Ruby/scripts/PlayerController.cs
@@ -10,6 +10,19 @@ public class PlayerController : MonoBehaviour
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
+    public float speed = 3.0f;
+
+
+    // Variables related to the health system
+    public int maxHealth = 5;
+    int currentHealth;
+    public int health { get { return currentHealth; } }
+
+
+    // Variables related to temporary invincibility
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float damageCooldown;
 
 
     // Start is called before the first frame update
@@ -17,19 +30,52 @@ public class PlayerController : MonoBehaviour
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
+
+
+        if (isInvincible)
+        {
+            damageCooldown -= Time.deltaTime;
+            if (damageCooldown < 0)
+            {
+                isInvincible = false;
+            }
+        }
     }
 
 
+    // FixedUpdate has the same call rate as the physics system
     void FixedUpdate()
     {
-        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
         rigidbody2d.MovePosition(position);
     }
+
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            damageCooldown = timeInvincible;
+        }
+
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+
 }
